621b5f3ac8854ad06bba37b43a6d20e5fc28b16f
Pitchayakit <pitchayakit.p@gmail.com>
2021-09-29 16:40:05 +0700

    Doenload files at detail page was fixed

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,15 +1,15 @@
 <template>
     <panel-item :field="field">
         <div slot="value">
-            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded inline-flex items-center"
+            <a :href="file.url" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded inline-flex items-center"
                 v-for="file in JSON.parse(field.value)"
-                @click="getFile(file.url, file.originalName)"
+                target="_blank"
             >
                 <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
                 </svg>
                 <span>{{ file.originalName }}</span>
-            </button>
+            </a>
         </div>
     </panel-item>
 </template>
